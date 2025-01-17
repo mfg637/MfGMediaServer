a1570552513dd486f43a1c737c5dd588c5b57ca6
mfg637 <mfg636@gmail.com>
2025-01-17 15:38:54 +0200

    automatic prompt extraction from images from civit.ai

--- medialib/upload.py
+++ medialib/upload.py
@@ -4,6 +4,7 @@ import pyimglib
 import magic
 import io
 import PIL.Image
+import PIL.ExifTags
 import medialib_db
 import datetime
 import random
@@ -11,8 +12,13 @@ import string
 import pathlib
 import pyimglib
 import pillow_heif
+import re
 
 from shared_code import EXTENSIONS_BY_MIME
+
+simple_id_pattern = re.compile("[a-z]{2}\d+")
+
+CIVIT_AI_ORIGIN = "civit ai"
 
 
 upload_blueprint = flask.Blueprint('upload', __name__, url_prefix='/upload')
@@ -35,14 +41,21 @@ def show_upload_page():
 @shared_code.login_validation
 def upload_file():
     file = flask.request.files["image-file"]
-    print("FILE", file)
+    description = None
+    if len(flask.request.form["description"]) > 0:
+        description = flask.request.form["description"]
+    origin_name = None
+    if len(flask.request.form["origin_name"]) > 0:
+        origin_name = flask.request.form["origin_name"]
+    origin_id = None
+    if len(flask.request.form["origin_id"]) > 0:
+        origin_id = flask.request.form["origin_id"]
     is_image = False
     file_buffer = io.BytesIO(file.stream.read(200 * 1024 * 1024))
     file.stream.seek(0)
     mime = magic.from_buffer(file_buffer.getvalue(), mime=True)
     if mime == "video/quicktime" and file.mimetype == "video/mp4":
         mime = "video/mp4"
-    print("MIME", mime)
     file_buffer.seek(0)
     if mime not in EXTENSIONS_BY_MIME:
         supported_formats = []
@@ -55,6 +68,13 @@ def upload_file():
         )
     # trim too long filename
     title = str(pathlib.Path(file.filename).stem[:MAX_TITLE_LENGTH])
+    if origin_name is None and simple_id_pattern.match(title) is not None and title[:2] == "ca":
+        origin_name = CIVIT_AI_ORIGIN
+    if origin_name == CIVIT_AI_ORIGIN:
+        if simple_id_pattern.match(title) is not None:
+            origin_id = title[2:]
+        elif re.match("\d+", title) is not None:
+            origin_id = title
     if mime.startswith("image/"):
         is_image = True
     hash = None
@@ -66,6 +86,13 @@ def upload_file():
             img = heif.to_pillow()
         else:
             img = PIL.Image.open(file_buffer)
+        if mime == "image/jpeg" and origin_name == CIVIT_AI_ORIGIN and description is None:
+            if "exif" in img.info:
+                exif = img.getexif()
+                exif_items = {k:v for k, v in exif.get_ifd(PIL.ExifTags.Base.ExifOffset).items()}
+                user_comment_raw: bytes = exif_items[37510]
+                description = user_comment_raw.decode("utf-16_be")[5:]
+
         hash = pyimglib.calc_image_hash(img)
         duplicates = medialib_db.find_content_by_hash(hash[1].hex().lower(), hash[2], hash[3], connection)
         if len(duplicates) > 0:
@@ -95,18 +122,6 @@ def upload_file():
     file_path = outdir.joinpath(filename)
     print("SAVE OUTPUT FILE", file_path)
     file.save(file_path)
-
-    description = None
-    if len(flask.request.form["description"]) > 0:
-        description = flask.request.form["description"]
-    
-    origin_name = None
-    if len(flask.request.form["origin_name"]) > 0:
-        origin_name = flask.request.form["origin_name"]
-    
-    origin_id = None
-    if len(flask.request.form["origin_id"]) > 0:
-        origin_id = flask.request.form["origin_id"]
 
     content_new_data = {
         'content_title': title,
