41e3bc3c01badcd7011614b89c7708c1022c9d9c
mfg637 <mfg636@gmail.com>
2025-01-25 12:24:28 +0200

    refactor file uploading code

--- medialib/upload.py
+++ medialib/upload.py
@@ -13,17 +13,74 @@ import pathlib
 import pyimglib
 import pillow_heif
 import re
+import dataclasses
 
 from shared_code import EXTENSIONS_BY_MIME
 
 simple_id_pattern = re.compile("[a-z]{2}\d+")
 
 CIVIT_AI_ORIGIN = "civit ai"
+MAX_TITLE_LENGTH = 63
+MAX_SAMPLE_LENGTH = 200 * 1024 * 1024 # 200 MiB
+MOV_MIMETYPE = "video/quicktime"
+MPEG4V_MIMETYPE = "video/mp4"
+JPEG_MIMETYPE = "image/jpeg"
+AVIF_MIMETYPE = "image/avif"
 
+def generate_unsupported_type_response():
+    supported_formats = []
+    for mime in EXTENSIONS_BY_MIME:
+        supported_formats.append(f"{EXTENSIONS_BY_MIME[mime]} ({mime})")
+    return flask.Response(
+        "Server accepts only this formats: " + \
+            ", ".join(supported_formats),
+        415
+    )
+
+def detect_file_type(file_buffer, request_header_mimetype):
+    mime = magic.from_buffer(file_buffer.getvalue(), mime=True)
+    if mime == MOV_MIMETYPE and request_header_mimetype == MPEG4V_MIMETYPE:
+        mime = MPEG4V_MIMETYPE
+    is_image = False
+    if mime.startswith("image/"):
+        is_image = True
+        file_type = None
+    if is_image:
+        file_type = "image"
+    elif mime.startswith("video/"):
+        file_type = "video"
+    elif mime.startswith("audio/"):
+        file_type = "audio"
+    else:
+        raise Exception("undetected content type")
+    return mime, file_type, is_image
+
+def detect_source(filename: str, origin_name: str | None, origin_id: str | None):
+    if origin_name is None and simple_id_pattern.match(filename) is not None and filename[:2] == "ca":
+        origin_name = CIVIT_AI_ORIGIN
+    if origin_name == CIVIT_AI_ORIGIN:
+        if simple_id_pattern.match(filename) is not None:
+            origin_id = filename[2:]
+        elif re.match("\d+", filename) is not None:
+            origin_id = filename
+    return origin_name, origin_id
+
+
+@dataclasses.dataclass
+class PlainTextData:
+    data: str
+
+
+def extract_metadata_from_image(img: PIL.Image.Image, mime: str, origin_name: str | None):
+    if mime == JPEG_MIMETYPE and origin_name == CIVIT_AI_ORIGIN:
+        if "exif" in img.info:
+            exif = img.getexif()
+            exif_items = {k:v for k, v in exif.get_ifd(PIL.ExifTags.Base.ExifOffset).items()}
+            user_comment_raw: bytes = exif_items[37510]
+            return PlainTextData(user_comment_raw.decode("utf-16_be")[5:])
+    return None
 
 upload_blueprint = flask.Blueprint('upload', __name__, url_prefix='/upload')
-
-MAX_TITLE_LENGTH = 63
 
 
 @upload_blueprint.route("/")
@@ -40,59 +97,40 @@ def show_upload_page():
 @upload_blueprint.route("/uploading", methods=["POST"])
 @shared_code.login_validation
 def upload_file():
+    def extract_fields() -> tuple[str | None, str | None, str | None]:
+        description = None
+        if len(flask.request.form["description"]) > 0:
+            description = flask.request.form["description"]
+        origin_name = None
+        if len(flask.request.form["origin_name"]) > 0:
+            origin_name = flask.request.form["origin_name"]
+        origin_id = None
+        if len(flask.request.form["origin_id"]) > 0:
+            origin_id = flask.request.form["origin_id"]
+        return description, origin_name, origin_id
+
     file = flask.request.files["image-file"]
-    description = None
-    if len(flask.request.form["description"]) > 0:
-        description = flask.request.form["description"]
-    origin_name = None
-    if len(flask.request.form["origin_name"]) > 0:
-        origin_name = flask.request.form["origin_name"]
-    origin_id = None
-    if len(flask.request.form["origin_id"]) > 0:
-        origin_id = flask.request.form["origin_id"]
-    is_image = False
-    file_buffer = io.BytesIO(file.stream.read(200 * 1024 * 1024))
+    description, origin_name, origin_id = extract_fields()
+    file_buffer = io.BytesIO(file.stream.read(MAX_SAMPLE_LENGTH))
     file.stream.seek(0)
-    mime = magic.from_buffer(file_buffer.getvalue(), mime=True)
-    if mime == "video/quicktime" and file.mimetype == "video/mp4":
-        mime = "video/mp4"
+    mime, file_type, is_image = detect_file_type(file_buffer, file.mimetype)
     file_buffer.seek(0)
     if mime not in EXTENSIONS_BY_MIME:
-        supported_formats = []
-        for mime in EXTENSIONS_BY_MIME:
-            supported_formats.append(f"{EXTENSIONS_BY_MIME[mime]} ({mime})")
-        return flask.Response(
-            "Server accepts only this formats: " + \
-                ", ".join(supported_formats),
-            415
-        )
+        return generate_unsupported_type_response()
     # trim too long filename
     title = str(pathlib.Path(file.filename).stem[:MAX_TITLE_LENGTH])
-    if origin_name is None and simple_id_pattern.match(title) is not None and title[:2] == "ca":
-        origin_name = CIVIT_AI_ORIGIN
-    if origin_name == CIVIT_AI_ORIGIN:
-        if simple_id_pattern.match(title) is not None:
-            origin_id = title[2:]
-        elif re.match("\d+", title) is not None:
-            origin_id = title
-    if mime.startswith("image/"):
-        is_image = True
+    origin_name, origin_id = detect_source(title, origin_name, origin_id)
     hash = None
     connection = medialib_db.common.make_connection()
     is_alternate_version = False
+    image_metadata = None
     if is_image:
-        if mime == "image/avif":
+        if mime == AVIF_MIMETYPE:
             heif = pillow_heif.open_heif(file_buffer)
             img = heif.to_pillow()
         else:
             img = PIL.Image.open(file_buffer)
-        if mime == "image/jpeg" and origin_name == CIVIT_AI_ORIGIN and description is None:
-            if "exif" in img.info:
-                exif = img.getexif()
-                exif_items = {k:v for k, v in exif.get_ifd(PIL.ExifTags.Base.ExifOffset).items()}
-                user_comment_raw: bytes = exif_items[37510]
-                description = user_comment_raw.decode("utf-16_be")[5:]
-
+        image_metadata = extract_metadata_from_image(img, mime, origin_name)
         hash = pyimglib.calc_image_hash(img)
         duplicates = medialib_db.find_content_by_hash(hash[1].hex().lower(), hash[2], hash[3], connection)
         if len(duplicates) > 0:
@@ -104,15 +142,8 @@ def upload_file():
             else:
                 is_alternate_version = True
     
-    file_type = None
-    if is_image:
-        file_type = "image"
-    elif mime.startswith("video/"):
-        file_type = "video"
-    elif mime.startswith("audio/"):
-        file_type = "audio"
-    else:
-        raise Exception("undetected content type")
+    if description is None and isinstance(image_metadata, PlainTextData):
+        description = image_metadata.data
     
     outdir = shared_code.get_output_directory()
     outdir.mkdir(parents=True, exist_ok=True)
